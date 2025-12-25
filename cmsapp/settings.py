dc4e0f330b7c245a3a29555389d5f243b013fd6e
Corey <cj@cjgee.com>
2025-12-24 19:27:28 -0600

    added vps IP to ALLOWED_HOSTS

--- cmsapp/settings.py
+++ cmsapp/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,172.238.164.179').split(',')
 
 # Application definition
 INSTALLED_APPS = [
